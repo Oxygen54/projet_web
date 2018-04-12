6cb62633f696aa3694564df7e2c61416fd21f5b7
Olivier <olivier.descoups@viacesi.fr>
2018-04-12 11:27:21 +0200

    V2

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/dlimage', function(){
+
+
+    return view('/cesi/dlimage'); //to the dl images page
+});
