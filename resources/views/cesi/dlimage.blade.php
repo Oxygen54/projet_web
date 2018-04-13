62b0d9763517a45e14c6c8505385e317785c7693
Olivier <olivier.descoups@viacesi.fr>
2018-04-13 10:12:17 +0200

    V2

--- resources/views/cesi/dlimage.blade.php
+++ resources/views/cesi/dlimage.blade.php
@@ -8,7 +8,7 @@ $files = glob("images/*.*");
 for ($i=1; $i<count($files); $i++)
 {
     $num = $files[$i];
-    echo '<img src="'.$num.'" alt="random image">'."&nbsp;&nbsp;";
+    echo '<img src="'.$num.'" alt="random image"  href="'.$num.'"  download>'."&nbsp;&nbsp;";
 }
 @endphp
 
