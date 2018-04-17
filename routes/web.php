bc85ff42ddc6ed2ec211b4699f837f5964d33bf4
Olivier <olivier.descoups@viacesi.fr>
2018-04-17 15:09:22 +0200

    V2

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -30,3 +21,6 @@ Route::get('/dlregistered', function(){
 
     return view('/sign/dlregistered'); //to the dl images page
 });
+
+
+
