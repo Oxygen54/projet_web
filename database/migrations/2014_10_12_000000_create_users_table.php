aa1778507b78d243b59a5ebeb5791ab2ff39d3bd
Olivier <olivier.descoups@viacesi.fr>
2018-04-18 15:47:23 +0200

    new branch right

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('rank');
+            $table->integer('rank')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
